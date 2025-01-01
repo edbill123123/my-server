b450b42a0c6e37e52c313b284c146b22133a63a3
Sue <edbill123123@gmail.com>
2025-01-02 01:44:12 +0800

    6

--- api/index.js
+++ api/index.js
@@ -1,15 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+
+// 假設這些是你導入的驗證方法
 const { sendPhoneCode, verifyPhoneCode, sendEmailCode, verifyEmailCode, verifyGoogleLogin, verifyFacebookLogin } = require("./verification");
 
 const app = express();
 
-// 中間件設定
+// 中間件
 app.use(cors({ origin: "*" }));
 app.use(bodyParser.json());
 
-// API 路由
+// 路由設置
+app.get("/", (req, res) => {
+  res.send("Hello, Express!");
+});
+
 app.post("/phone/sendCode", sendPhoneCode);
 app.post("/phone/verifyCode", verifyPhoneCode);
 app.post("/email/sendCode", sendEmailCode);
@@ -17,5 +23,5 @@ app.post("/email/verifyCode", verifyEmailCode);
 app.post("/auth/google", verifyGoogleLogin);
 app.post("/auth/facebook", verifyFacebookLogin);
 
-// 模擬運行應用（不需要 listen）
+// 將 Express 應用導出，Vercel 將使用此應用處理請求
 module.exports = app;
